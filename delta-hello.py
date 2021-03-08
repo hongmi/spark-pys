90d5e87620eeab003a9ae9f3f80f0a3ba481bfbb
hongmin <lispmm@outlook.com>
2021-03-08 11:28:57 +0800

    aaa

--- delta-hello.py
+++ delta-hello.py
@@ -31,6 +31,9 @@ if __name__ == "__main__":
     spark = SparkSession\
         .builder\
         .appName("PythonPi")\
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:0.8.0") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .getOrCreate()
 
     partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
