4d9b326127bc7bf6153bcce35a08a7248ccb2a51
hongmin <lispmm@outlook.com>
2021-03-08 18:44:10 +0800

    aa

--- delta-hello.py
+++ delta-hello.py
@@ -28,9 +28,6 @@ if __name__ == "__main__":
     spark = SparkSession\
         .builder\
         .appName("PythonPi")\
-        .config("spark.jars.packages", "io.delta:delta-core_2.11:0.6.1") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .getOrCreate()
 
     from delta.tables import *
